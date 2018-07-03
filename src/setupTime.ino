b07953c3ef92b9b1d52d3dc0189f0b005cad039e
Mike Sowter <mike@sowter.com>
2018-07-03 17:05:56 +1000

    connected ads1115, has issues

--- src/setupTime.ino
+++ src/setupTime.ino
@@ -8,7 +8,7 @@ void setupTime() {
   oldHour = hour();
   if (hour()<7||hour()>=22) T31time = true;
   else T31time = false;
-  strcpy(todayName,"/PR");
+  strcpy(todayName,"/em");
   strcat(todayName,dateStamp());
   strcat(todayName,".csv");
 }
