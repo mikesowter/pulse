05eeeeca1a96e39fab13070d9055b1c6e77ab06b
Mike Sowter <mike@sowter.com>
2022-07-02 20:35:58 +1000

    increased HOTWATER

--- src/constants.h
+++ src/constants.h
@@ -4,7 +4,7 @@
 #define BUFFER_SIZE 128
 #define ISR_CAP 127
 #define TIMEZONE 10
-#define HOT_WATER 3.3
+#define HOT_WATER 3.6   // update from 3.3 on 2022/07/02  17:40
 #define LDR 4
 #define BLU 12
 #define GRN 13
