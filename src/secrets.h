a9cf41744ee1b0786801a86fcb45de515419bc54
Mike Sowter <mike@sowter.com>
2024-05-25 10:50:05 +1000

    connect meter to garage wifi

--- src/secrets.h
+++ src/secrets.h
@@ -1,4 +1,6 @@
-char ssid[] = "ZombiesAteMyBrains";   //  your network SSID (name)
-char pass[] = "iron1951";
+//char ssid[] = "ZombiesAteMyBrains";   //  your network SSID (name)
+//char pass[] = "iron1951";
 //char ssid[] = "TelstraCF6EC7";
-//char pass[] = "meauff8qqwn9";           //  your network password
+//char pass[] = "meauff8qqwn9";           
+char ssid[] = "garage";   
+char pass[] = "iron1951";
